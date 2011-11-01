795cee4dcc19bb94d770438abb93ed4aff82f774
Yiorgis Gozadinos <ggozad@jarn.com>
2011-11-01 16:08:25 +0100

    Fix carelessness

--- jarn/jsi18n/view.py
+++ jarn/jsi18n/view.py
@@ -1,13 +1,19 @@
 import json
 
-from Products.Five.browser import BrowserView
+from plone.memoize import ram
 from zope.component import queryUtility
 from zope.i18n.interfaces import ITranslationDomain
+
+from Products.Five.browser import BrowserView
+
+
+def _cache_key(method, self, domain, language):
+    return (domain, language,)
 
 
 class i18njs(BrowserView):
 
-    @ram.cache(lambda method, domain, language: domain+language)
+    @ram.cache(_cache_key)
     def _gettext_catalog(self, domain, language):
         td = queryUtility(ITranslationDomain, domain)
         if td is None or language not in td._catalogs:
